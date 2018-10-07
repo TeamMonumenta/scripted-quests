314915c3cc1ea8768bdbcdfeb948ce3f0da5ccec
Byron Marohn <combustible@live.com>
2018-10-06 19:51:41 -0700

    Updated interactnpc to new CommandAPI target selectors
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugin/src/main/java/com/playmonumenta/scriptedquests/commands/InteractNpc.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/commands/InteractNpc.java
@@ -3,11 +3,14 @@ package com.playmonumenta.scriptedquests.commands;
 import com.playmonumenta.scriptedquests.Plugin;
 
 import io.github.jorelali.commandapi.api.arguments.Argument;
+import io.github.jorelali.commandapi.api.arguments.EntitySelectorArgument;
+import io.github.jorelali.commandapi.api.arguments.EntitySelectorArgument.EntitySelector;
 import io.github.jorelali.commandapi.api.arguments.EntityTypeArgument;
 import io.github.jorelali.commandapi.api.arguments.StringArgument;
-import io.github.jorelali.commandapi.api.CommandPermission;
 import io.github.jorelali.commandapi.api.CommandAPI;
+import io.github.jorelali.commandapi.api.CommandPermission;
 
+import java.util.Collection;
 import java.util.LinkedHashMap;
 
 import org.bukkit.ChatColor;
@@ -16,45 +19,47 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
 public class InteractNpc {
+	@SuppressWarnings("unchecked")
 	public static void register(Plugin plugin) {
 		/* First one of these has both required arguments */
 		LinkedHashMap<String, Argument> arguments = new LinkedHashMap<>();
 
+		arguments.put("players", new EntitySelectorArgument(EntitySelector.MANY_PLAYERS));
 		arguments.put("npcName", new StringArgument());
 		arguments.put("npcType", new EntityTypeArgument());
 
 		CommandAPI.getInstance().register("interactnpc",
 		                                  new CommandPermission("scriptedquests.interactnpc"),
 		                                  arguments,
-										  (sender, args) -> {
-			                                  interact(plugin, sender, (String)args[0], (EntityType)args[1]);
+		                                  (sender, args) -> {
+		                                      interact(plugin, sender, (Collection<Player>)args[0],
+		                                               (String)args[1], (EntityType)args[2]);
 		                                  }
 		);
 
 		/* Second one just has the npc name with VILLAGER as default */
 		arguments = new LinkedHashMap<>();
 
+		arguments.put("players", new EntitySelectorArgument(EntitySelector.MANY_PLAYERS));
 		arguments.put("npcName", new StringArgument());
 
 		CommandAPI.getInstance().register("interactnpc",
 		                                  new CommandPermission("scriptedquests.interactnpc"),
 		                                  arguments,
-										  (sender, args) -> {
-			                                  interact(plugin, sender, (String)args[0], EntityType.VILLAGER);
+		                                  (sender, args) -> {
+		                                      interact(plugin, sender, (Collection<Player>)args[0],
+		                                               (String)args[1], EntityType.VILLAGER);
 		                                  }
 		);
 	}
 
-	private static void interact(Plugin plugin, CommandSender sender, String npcName, EntityType npcType) {
-		if (sender instanceof Player) {
-			Player player = (Player) sender;
-
+	private static void interact(Plugin plugin, CommandSender sender, Collection<Player>players,
+	                             String npcName, EntityType npcType) {
+		for (Player player : players) {
 			if (!plugin.mNpcManager.interactEvent(plugin, player, npcName, npcType, true)) {
 				sender.sendMessage(ChatColor.RED + "No interaction available for player '" + player.getName() +
 				                   "' and NPC '" + npcName + "'");
 			}
-		} else {
-			sender.sendMessage("This command must be run by/on a player!");
 		}
 	}
 }
