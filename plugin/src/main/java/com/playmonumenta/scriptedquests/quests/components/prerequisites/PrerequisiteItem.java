b48edeb3a8384fc70ef224b3e58d60f70eee7c2b
Byron Marohn <combustible@live.com>
2018-10-20 19:06:00 -0700

    Refactor item testing to use shared code
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/quests/components/prerequisites/PrerequisiteItem.java
@@ -0,0 +1,97 @@
+package com.playmonumenta.scriptedquests.quests;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import com.playmonumenta.scriptedquests.utils.InventoryUtils;
+
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.Material;
+
+class PrerequisiteItem {
+	private String mName = "";
+	private String mLore = "";
+	private Material mType = Material.AIR;
+	private int mCount = 1;
+
+	PrerequisiteItem(JsonElement element) throws Exception {
+		JsonObject object = element.getAsJsonObject();
+		if (object == null) {
+			throw new Exception("item value is not an object!");
+		}
+
+		Set<Entry<String, JsonElement>> entries = object.entrySet();
+		for (Entry<String, JsonElement> ent : entries) {
+			String key = ent.getKey();
+			JsonElement value = ent.getValue();
+
+			if (!key.equals("lore") && !key.equals("name") && !key.equals("count") && !key.equals("type")) {
+				throw new Exception("Unknown item key: " + key);
+			}
+
+			if (key.equals("lore")) {
+				mLore = value.getAsString();
+				if (mLore == null) {
+					throw new Exception("item lore entry is not a string!");
+				}
+			} else if (key.equals("name")) {
+				mName = value.getAsString();
+				if (mName == null) {
+					throw new Exception("item name entry is not a string!");
+				}
+			} else if (key.equals("count")) {
+				mCount = value.getAsInt();
+			} else if (key.equals("type")) {
+				String typeStr = value.getAsString();
+				if (typeStr == null) {
+					throw new Exception("item type entry is not a string!");
+				}
+				try {
+					mType = Material.valueOf(typeStr);
+				} catch (IllegalArgumentException e) {
+					throw new Exception("Unknown Material '" + typeStr +
+					                    "' - it should be one of the values in this list: " +
+					                    "https://hub.spigotmc.org/javadocs/spigot/org/bukkit/Material.html");
+				}
+			}
+		}
+	}
+
+	/* Note this is not the standard check - can't be used as a generic PrerequisiteBase */
+	public boolean prerequisiteMet(ItemStack[] items) {
+		int matchCount = 0;
+
+		if (items != null) {
+			//  Loop through the given items
+			for (ItemStack item : items) {
+				if (item == null) {
+					continue;
+				}
+
+				if (InventoryUtils.testForItemWithName(item, mName) &&
+				    InventoryUtils.testForItemWithLore(item, mLore) &&
+				    (mType.equals(Material.AIR) || mType.equals(item.getType()))) {
+					matchCount += item.getAmount();
+				}
+
+				if (mCount <= 0 && matchCount > 0) {
+					// Found an item where none should be - fail
+					return false;
+				} else if (mCount > 0 && matchCount >= mCount) {
+					// Found at least the correct number of items
+					return true;
+				}
+			}
+		}
+
+		// didn't find item when didn't expect to
+		if (mCount <= 0 && matchCount <= 0) {
+			return true;
+		}
+
+		return false;
+	}
+}
