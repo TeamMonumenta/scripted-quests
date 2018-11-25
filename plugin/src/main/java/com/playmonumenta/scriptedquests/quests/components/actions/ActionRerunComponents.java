84b044fbe06a12f920b0b05472ada4133307803c
Byron Marohn <combustible@live.com>
2018-11-25 12:27:40 -0800

    Fix rerun_components blocking triggers for multiple simultaneous people
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugin/src/main/java/com/playmonumenta/scriptedquests/quests/components/actions/ActionRerunComponents.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/quests/components/actions/ActionRerunComponents.java
@@ -1,14 +1,19 @@
 package com.playmonumenta.scriptedquests.quests;
 
+import com.playmonumenta.scriptedquests.Plugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
-import com.playmonumenta.scriptedquests.Plugin;
-
 class ActionRerunComponents implements ActionBase {
-	private String mNpcName;
-	private EntityType mEntityType;
-	private boolean mLocked = false;
+	private final String mNpcName;
+	private final EntityType mEntityType;
+
+	// Array list should be faster than hashmap for such small set
+	private final List<Player> mLocked = new ArrayList<Player>(10);
 
 	ActionRerunComponents(String npcName, EntityType entityType) {
 		mNpcName = npcName;
@@ -21,10 +26,10 @@ class ActionRerunComponents implements ActionBase {
 		 * Prevent infinite loops by preventing this specific action
 		 * from running itself again
 		 */
-		if (!mLocked) {
-			mLocked = true;
+		if (!mLocked.contains(player)) {
+			mLocked.add(player);
 			plugin.mNpcManager.interactEvent(plugin, player, mNpcName, mEntityType, true);
-			mLocked = false;
+			mLocked.remove(player);
 		} else {
 			plugin.getLogger().severe("Stopped infinite loop for NPC '" + mNpcName + "'");
 		}
