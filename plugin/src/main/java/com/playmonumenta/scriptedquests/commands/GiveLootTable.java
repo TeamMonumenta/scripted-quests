12ff7cbf355c06d0154cc2ac359ed4b671c34082
Byron Marohn <combustible@live.com>
2019-08-16 18:16:28 -0700

    /giveloottable now also takes an optional integer count
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugin/src/main/java/com/playmonumenta/scriptedquests/commands/GiveLootTable.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/commands/GiveLootTable.java
@@ -15,31 +15,48 @@ import io.github.jorelali.commandapi.api.CommandPermission;
 import io.github.jorelali.commandapi.api.arguments.Argument;
 import io.github.jorelali.commandapi.api.arguments.EntitySelectorArgument;
 import io.github.jorelali.commandapi.api.arguments.EntitySelectorArgument.EntitySelector;
+import io.github.jorelali.commandapi.api.arguments.IntegerArgument;
 import io.github.jorelali.commandapi.api.arguments.TextArgument;
 
 public class GiveLootTable {
 	@SuppressWarnings("unchecked")
 	public static void register(Random random) {
+		CommandPermission perms = CommandPermission.fromString("scriptedquests.giveloottable");
 		LinkedHashMap<String, Argument> arguments = new LinkedHashMap<>();
 
 		arguments.put("players", new EntitySelectorArgument(EntitySelector.MANY_PLAYERS));
 		arguments.put("lootTablePath", new TextArgument());
 
 		CommandAPI.getInstance().register("giveloottable",
-		                                  CommandPermission.fromString("scriptedquests.giveloottable"),
+		                                  perms,
 		                                  arguments,
 		                                  (sender, args) -> {
-		                                      String lootPath = (String)args[1];
-		                                      for (Player player : (Collection<Player>)args[0]) {
-		                                          try {
-		                                              InventoryUtils.giveLootTableContents(player, lootPath, random);
-		                                          } catch (Exception e) {
-		                                              player.sendMessage(ChatColor.RED + "BUG! Server failed to give you loot from the table '" + lootPath + "'");
-		                                              player.sendMessage(ChatColor.RED + "Please hover over the following message, take a screenshot, and report this to a moderator");
-		                                              MessagingUtils.sendStackTrace(player, e);
-		                                          }
-		                                      }
+											  giveLoot((Collection<Player>)args[0], (String)args[1], 1, random);
 		                                  }
 		);
+
+		arguments.put("count", new IntegerArgument(1, 1000));
+
+		CommandAPI.getInstance().register("giveloottable",
+		                                  perms,
+		                                  arguments,
+		                                  (sender, args) -> {
+											  giveLoot((Collection<Player>)args[0], (String)args[1], (Integer)args[2], random);
+		                                  }
+		);
+	}
+
+	private static void giveLoot(Collection<Player> players, String lootPath, int count, Random random) {
+		for (Player player : players) {
+			try {
+				for (int i = 0; i < count; i++) {
+					InventoryUtils.giveLootTableContents(player, lootPath, random);
+				}
+			} catch (Exception e) {
+				player.sendMessage(ChatColor.RED + "BUG! Server failed to give you loot from the table '" + lootPath + "'");
+				player.sendMessage(ChatColor.RED + "Please hover over the following message, take a screenshot, and report this to a moderator");
+				MessagingUtils.sendStackTrace(player, e);
+			}
+		}
 	}
 }
