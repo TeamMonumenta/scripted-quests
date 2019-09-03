2d036b73c5cdcc435ce871547b63e68751b035f1
Byron Marohn <combustible@live.com>
2019-09-03 09:54:42 -0700

    Move CommandTimerInstance to its own file
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugin/src/main/java/com/playmonumenta/scriptedquests/timers/CommandTimer.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/timers/CommandTimer.java
@@ -1,145 +1,22 @@
 package com.playmonumenta.scriptedquests.timers;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
-import org.bukkit.block.CommandBlock;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import com.playmonumenta.scriptedquests.Plugin;
 
 public class CommandTimer implements Listener {
-	private static class CommandTimerInstance {
-		private final Location mLoc;
-		private final String mPeriodStr;
-		private final int mPlayerRange;
-		private final boolean mRepeat;
-
-		/*
-		 * The last state a repeater was set to - only used for repeaters
-		 *
-		 * Default to "on" so the first thing that happens after loading
-		 * is to turn it off if no players in range
-		 */
-		private boolean mRepeaterEnabled = true;
-
-		private CommandTimerInstance(Location loc, String periodStr, int playerRange, boolean repeat) {
-			mLoc = loc;
-			mPeriodStr = periodStr;
-			mPlayerRange = playerRange;
-			mRepeat = repeat;
-		}
-
-		private boolean canRun() {
-			if (mPlayerRange <= 0) {
-				return true;
-			}
-
-			for (Player player : Bukkit.getOnlinePlayers()) {
-				if (mLoc.distance(player.getLocation()) <= mPlayerRange && player.getGameMode() != GameMode.SPECTATOR) {
-					return true;
-				}
-			}
-			return false;
-		}
-
-		private void tick(Plugin plugin) {
-			if (canRun()) {
-				setAutoState(plugin, mLoc, true);
-				mRepeaterEnabled = true;
-			} else if (mRepeat && mRepeaterEnabled) {
-				/* Turn repeaters back off again when player is out of range */
-				setAutoState(plugin, mLoc, false);
-				mRepeaterEnabled = false;
-			}
-		}
-
-		private void unload(Plugin plugin) {
-			if (mRepeat && mRepeaterEnabled) {
-				/* Turn repeaters back off when unloading timer */
-				setAutoState(plugin, mLoc, false);
-				mRepeaterEnabled = false;
-			}
-		}
-
-		private void setName(ArmorStand entity) {
-			entity.setCustomName(getName());
-		}
-
-		private String getName() {
-			String name = "";
-			if (mRepeat) {
-				name += ChatColor.LIGHT_PURPLE + "Repeater ";
-			} else {
-				name += ChatColor.GOLD + "Timer ";
-			}
-			name += mPeriodStr + " ";
-			if (mPlayerRange <= 0) {
-				name += ChatColor.DARK_PURPLE + "always ";
-			} else if (mPlayerRange > 1000) {
-				name += ChatColor.YELLOW + "range=" + Integer.toString(mPlayerRange);
-			} else {
-				name += ChatColor.GREEN + "range=" + Integer.toString(mPlayerRange);
-			}
-			return name;
-		}
-
-		public String toString() {
-			return Integer.toString(mLoc.getBlockX()) + " " +
-			       Integer.toString(mLoc.getBlockY()) + " " +
-			       Integer.toString(mLoc.getBlockZ()) + " " +
-			       getName();
-		}
-
-		private static java.lang.reflect.Method cachedHandleMethod = null;
-		private static java.lang.reflect.Method cachedAutoMethod = null;
-		private static void setAutoState(Plugin plugin, Location loc, boolean auto) {
-			Block block = loc.getBlock();
-			BlockState state = block.getState();
-			if (state instanceof CommandBlock) {
-				/*
-				 * Use reflection to:
-				 *   TileEntityCommand nmsTileCmd = cmd.getTileEntity();
-				 *   nmsTileCmd.b(true)
-				 */
-				try {
-					// Get command block's getTileEntity() via reflection
-					// Cache reflection results for performance
-					if (cachedHandleMethod == null) {
-						cachedHandleMethod = state.getClass().getMethod("getTileEntity");
-					}
-					Object handle = cachedHandleMethod.invoke(state);
-
-					if (cachedAutoMethod == null) {
-						cachedAutoMethod = handle.getClass().getMethod("b", boolean.class);
-					}
-					cachedAutoMethod.invoke(handle, auto);
-				} catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-					plugin.getLogger().severe("Failed to set command block auto state at " + loc.toString());
-					e.printStackTrace();
-				}
-			} else {
-				plugin.getLogger().severe("Command block is missing for timer at " + loc.toString());
-				return;
-			}
-		}
-	}
-
 	private final Plugin mPlugin;
 	private final Map<UUID, CommandTimerInstance> mTimers;
 	private final BukkitRunnable mRunnable;
---
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/timers/CommandTimerInstance.java
@@ -0,0 +1,132 @@
+package com.playmonumenta.scriptedquests.timers;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.CommandBlock;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Player;
+
+import com.playmonumenta.scriptedquests.Plugin;
+
+public class CommandTimerInstance {
+	private final Location mLoc;
+	private final String mPeriodStr;
+	private final int mPlayerRange;
+	private final boolean mRepeat;
+
+	/*
+	 * The last state a repeater was set to - only used for repeaters
+	 *
+	 * Default to "on" so the first thing that happens after loading
+	 * is to turn it off if no players in range
+	 */
+	private boolean mRepeaterEnabled = true;
+
+	public CommandTimerInstance(Location loc, String periodStr, int playerRange, boolean repeat) {
+		mLoc = loc;
+		mPeriodStr = periodStr;
+		mPlayerRange = playerRange;
+		mRepeat = repeat;
+	}
+
+	public boolean canRun() {
+		if (mPlayerRange <= 0) {
+			return true;
+		}
+
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			if (mLoc.distance(player.getLocation()) <= mPlayerRange && player.getGameMode() != GameMode.SPECTATOR) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void tick(Plugin plugin) {
+		if (canRun()) {
+			setAutoState(plugin, mLoc, true);
+			mRepeaterEnabled = true;
+		} else if (mRepeat && mRepeaterEnabled) {
+			/* Turn repeaters back off again when player is out of range */
+			setAutoState(plugin, mLoc, false);
+			mRepeaterEnabled = false;
+		}
+	}
+
+	public void unload(Plugin plugin) {
+		if (mRepeat && mRepeaterEnabled) {
+			/* Turn repeaters back off when unloading timer */
+			setAutoState(plugin, mLoc, false);
+			mRepeaterEnabled = false;
+		}
+	}
+
+	public void setName(ArmorStand entity) {
+		entity.setCustomName(getName());
+	}
+
+	private String getName() {
+		String name = "";
+		if (mRepeat) {
+			name += ChatColor.LIGHT_PURPLE + "Repeater ";
+		} else {
+			name += ChatColor.GOLD + "Timer ";
+		}
+		name += mPeriodStr + " ";
+		if (mPlayerRange <= 0) {
+			name += ChatColor.DARK_PURPLE + "always ";
+		} else if (mPlayerRange > 1000) {
+			name += ChatColor.YELLOW + "range=" + Integer.toString(mPlayerRange);
+		} else {
+			name += ChatColor.GREEN + "range=" + Integer.toString(mPlayerRange);
+		}
+		return name;
+	}
+
+	public String toString() {
+		return Integer.toString(mLoc.getBlockX()) + " " +
+			   Integer.toString(mLoc.getBlockY()) + " " +
+			   Integer.toString(mLoc.getBlockZ()) + " " +
+			   getName();
+	}
+
+	private static java.lang.reflect.Method cachedHandleMethod = null;
+	private static java.lang.reflect.Method cachedAutoMethod = null;
+	private static void setAutoState(Plugin plugin, Location loc, boolean auto) {
+		Block block = loc.getBlock();
+		BlockState state = block.getState();
+		if (state instanceof CommandBlock) {
+			/*
+			 * Use reflection to:
+			 *   TileEntityCommand nmsTileCmd = cmd.getTileEntity();
+			 *   nmsTileCmd.b(true)
+			 */
+			try {
+				// Get command block's getTileEntity() via reflection
+				// Cache reflection results for performance
+				if (cachedHandleMethod == null) {
+					cachedHandleMethod = state.getClass().getMethod("getTileEntity");
+				}
+				Object handle = cachedHandleMethod.invoke(state);
+
+				if (cachedAutoMethod == null) {
+					cachedAutoMethod = handle.getClass().getMethod("b", boolean.class);
+				}
+				cachedAutoMethod.invoke(handle, auto);
+			} catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+				plugin.getLogger().severe("Failed to set command block auto state at " + loc.toString());
+				e.printStackTrace();
+			}
+		} else {
+			plugin.getLogger().severe("Command block is missing for timer at " + loc.toString());
+			return;
+		}
+	}
+}
+
