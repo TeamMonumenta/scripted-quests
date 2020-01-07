8886bf5a2ab09de13976ff2acb16860c6ecd5168
NickNackGus <NickNackGus@gmail.com>
2020-01-06 20:59:32 -0500

    Implemented BaseZone and the majority of ZoneFragment

---
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/zones/zone/BaseZone.java
@@ -0,0 +1,179 @@
+package com.playmonumenta.scriptedquests.zones.zone;
+
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+public class BaseZone implements Cloneable {
+	protected Vector mPosition;
+	protected Vector mSize;
+
+	public BaseZone(BaseZone other) throws Exception {
+		if (other == null) {
+			throw new Exception("other may not be null.");
+		}
+
+		mPosition = other.mPosition.clone();
+		mSize = other.mSize.clone();
+	}
+
+	/*
+	 * pos1 and pos2 are used similar to /fill:
+	 * - Both are inclusive coordinates.
+	 * - The minimum/maximum are determined for you.
+	 */
+	public BaseZone(Vector pos1, Vector pos2) throws Exception {
+		if (pos1 == null) {
+			throw new Exception("pos1 may not be null.");
+		}
+
+		if (pos2 == null) {
+			throw new Exception("pos2 may not be null.");
+		}
+
+		Vector posMin = Vector.getMinimum(pos1, pos2);
+		Vector posMax = Vector.getMaximum(pos1, pos2);
+
+		mPosition = posMin;
+		mSize = posMax.clone();
+		mSize.subtract(posMin);
+		mSize.add(new Vector(1, 1, 1));
+	}
+
+	@Override
+	public BaseZone clone() throws CloneNotSupportedException {
+		try {
+			return new BaseZone(this);
+		} catch (CloneNotSupportedException e) {
+			throw new Error(e);
+		} catch (Exception e) {
+			throw new CloneNotSupportedException("Cannot clone null object.");
+		}
+	}
+
+	/*
+	 * Returns the smallest integer coordinate that is inside the zone on each axis.
+	 */
+	public Vector minCorner() {
+		return mPosition.clone();
+	}
+
+	/*
+	 * Sets the smallest integer coordinate that is inside the zone on each axis.
+	 */
+	public void minCorner(Vector other) throws Exception {
+		if (other == null) {
+			throw new Exception("other may not be null.");
+		}
+
+		mSize.add(mPosition);
+		mSize.subtract(other);
+		mPosition = other.clone();
+	}
+
+	/*
+	 * Returns the largest integer coordinate that is inside the zone on each axis.
+	 */
+	public Vector maxCorner() {
+		Vector result = mPosition.clone();
+		result.add(mSize);
+		return result;
+	}
+
+	/*
+	 * Sets the largest integer coordinate that is inside the zone on each axis.
+	 */
+	public void maxCorner(Vector other) throws Exception {
+		if (other == null) {
+			throw new Exception("other may not be null.");
+		}
+
+		mSize.add(other);
+		mSize.subtract(mPosition);
+	}
+
+	/*
+	 * Returns the smallest coordinate that is outside the zone on each axis.
+	 */
+	public Vector trueMaxCorner() {
+		Vector result = maxCorner();
+		result.add(new Vector(1, 1, 1));
+		return result;
+	}
+
+	/*
+	 * Sets the smallest coordinate that is outside the zone on each axis.
+	 */
+	public void trueMaxCorner(Vector other) throws Exception {
+		if (other == null) {
+			throw new Exception("other may not be null.");
+		}
+
+		maxCorner(other);
+	}
+
+	public Vector size() {
+		return mSize.clone();
+	}
+
+	public double volume() {
+		return mSize.getX() * mSize.getY() * mSize.getZ();
+	}
+
+	/*
+	 * True if the size on each axis is > 0, else false.
+	 */
+	public boolean isValid() {
+		return mSize.getX() > 0 && mSize.getY() > 0 && mSize.getZ() > 0;
+	}
+
+	public boolean within(Vector loc) throws Exception {
+		if (loc == null) {
+			throw new Exception("loc may not be null.");
+		}
+
+		double x = loc.getX();
+		double y = loc.getY();
+		double z = loc.getZ();
+
+		Vector min = minCorner();
+		Vector max = trueMaxCorner();
+
+		return x >= min.getX() && x < max.getX() && y >= min.getY() && y < max.getY() && z >= min.getZ() && z < max.getZ();
+	}
+
+	public boolean within(Location loc) throws Exception {
+		if (loc == null) {
+			throw new Exception("loc may not be null.");
+		}
+
+		return within(loc.toVector());
+	}
+
+	/*
+	 * Returns a BaseZone that is inside this and the other zone.
+	 */
+	public BaseZone overlappingZone(BaseZone other) throws Exception {
+		if (other == null) {
+			throw new Exception("other may not be null.");
+		}
+
+		Vector selfMin = minCorner();
+		Vector selfMax = maxCorner();
+		Vector otherMin = other.minCorner();
+		Vector otherMax = other.maxCorner();
+
+		if (selfMax.getX() < otherMin.getX() ||
+		    selfMax.getY() < otherMin.getY() ||
+		    selfMax.getZ() < otherMin.getZ() ||
+		    otherMax.getX() < selfMin.getX() ||
+		    otherMax.getY() < selfMin.getY() ||
+		    otherMax.getZ() < selfMin.getZ()) {
+			return null;
+		}
+
+		Vector resultMin = Vector.getMaximum(selfMin, otherMin);
+		Vector resultMax = Vector.getMinimum(selfMax, otherMax);
+
+		return new BaseZone(resultMin, resultMax);
+	}
+}
---
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/zones/zone/ZoneFragment.java
@@ -0,0 +1,209 @@
+package com.playmonumenta.scriptedquests.zones.zone;
+
+import java.util.ArrayList;
+
+import org.bukkit.Axis;
+import org.bukkit.util.Vector;
+
+/*
+ * A fragment of a zone; this is used to find zones quickly, but not hold their properties.
+ * Instead, each fragment points to its parent, a zone with properties.
+ * Each zone also keeps track of its fragments.
+ */
+public class ZoneFragment extends BaseZone {
+	// TODO This should be Zone, not BaseZone
+	protected BaseZone mParent;
+
+	public ZoneFragment(ZoneFragment other) throws Exception {
+		super(other);
+		mParent = other.mParent;
+	}
+
+	// TODO This should be Zone, not BaseZone
+	public ZoneFragment(BaseZone other) throws Exception {
+		super(other);
+		mParent = other;
+	}
+
+	@Override
+	public ZoneFragment clone() throws CloneNotSupportedException {
+		try {
+			return new ZoneFragment(this);
+		} catch (CloneNotSupportedException e) {
+			throw new Error(e);
+		} catch (Exception e) {
+			throw new CloneNotSupportedException("Cannot clone null object.");
+		}
+	}
+
+	/*
+	 * Returns (lower_zone, upper_zone) for this split along some axis.
+	 *
+	 * Either zone may have a size of 0, and should be ignored.
+	 */
+	public ZoneFragment[] splitAxis(Vector pos, Axis axis) throws Exception {
+		ZoneFragment[] result = new ZoneFragment[2];
+
+		ZoneFragment lower;
+		ZoneFragment upper;
+
+		try {
+			lower = new ZoneFragment(this);
+			upper = new ZoneFragment(this);
+		} catch (Exception e) {
+			throw e;
+		}
+
+		Vector lowerMax = lower.maxCorner();
+		Vector upperMin = upper.minCorner();
+
+		switch(axis) {
+		case X:
+			lowerMax.setX(pos.getX());
+			upperMin.setX(pos.getX());
+			break;
+		case Y:
+			lowerMax.setX(pos.getX());
+			upperMin.setX(pos.getX());
+			break;
+		case Z:
+			lowerMax.setX(pos.getX());
+			upperMin.setX(pos.getX());
+			break;
+		default:
+			throw new Exception("Unknown axis.");
+		}
+
+		lower.maxCorner(lowerMax);
+		upper.minCorner(upperMin);
+
+		result[0] = lower;
+		result[1] = upper;
+
+		return result;
+	}
+
+	/*
+	 * Returns a list of fragments of this zone, split by an overlapping zone.
+	 */
+	public ArrayList<ZoneFragment> splitByOverlap(BaseZone overlap) throws Exception {
+		if (overlap == null) {
+			throw new Exception("overlap may not be null.");
+		}
+
+		ZoneFragment centerZone = new ZoneFragment(this);
+
+		Vector otherMin = overlap.minCorner();
+		Vector otherMax = overlap.trueMaxCorner();
+
+		ZoneFragment[] tempSplitResult;
+		ArrayList<ZoneFragment> result = new ArrayList<ZoneFragment>();
+
+		for (Axis axis : Axis.values()) {
+			ZoneFragment lower;
+			ZoneFragment upper;
+
+			ArrayList<ZoneFragment> workZones = result;
+			result = new ArrayList<ZoneFragment>();
+
+			for (ZoneFragment workZone : workZones) {
+				// Add zones split from existing split zones
+				tempSplitResult = workZone.splitAxis(otherMin, axis);
+				lower = tempSplitResult[0];
+				workZone = tempSplitResult[1];
+
+				tempSplitResult = workZone.splitAxis(otherMax, axis);
+				workZone = tempSplitResult[0];
+				upper = tempSplitResult[1];
+
+				if (lower.isValid()) {
+					result.add(lower);
+				}
+				if (workZone.isValid()) {
+					result.add(workZone);
+				}
+				if (upper.isValid()) {
+					result.add(upper);
+				}
+			}
+
+			// Add zones split from center, but not the center (overlap) itself
+			tempSplitResult = centerZone.splitAxis(otherMin, axis);
+			lower = tempSplitResult[0];
+			centerZone = tempSplitResult[1];
+
+			tempSplitResult = centerZone.splitAxis(otherMax, axis);
+			centerZone = tempSplitResult[0];
+			upper = tempSplitResult[1];
+
+			if (lower.isValid()) {
+				result.add(lower);
+			}
+			if (upper.isValid()) {
+				result.add(upper);
+			}
+		}
+
+		return result;
+	}
+
+	/*
+	 * Merge two ZoneFragments without changing their combined size/shape.
+	 *
+	 * Returns the merged ZoneFragment or None.
+	 */
+	public ZoneFragment merge(ZoneFragment other) throws Exception {
+		Vector aMin = minCorner();
+		Vector aMax = maxCorner();
+		Vector bMin = other.minCorner();
+		Vector bMax = other.maxCorner();
+
+		boolean xMatches = aMin.getX() == bMin.getX() && aMax.getX() == bMax.getX();
+		boolean yMatches = aMin.getY() == bMin.getY() && aMax.getY() == bMax.getY();
+		boolean zMatches = aMin.getZ() == bMin.getZ() && aMax.getZ() == bMax.getZ();
+
+		// Confirm at least 2/3 axes match
+		int matchingAxes = 0;
+		matchingAxes += xMatches ? 1 : 0;
+		matchingAxes += yMatches ? 1 : 0;
+		matchingAxes += zMatches ? 1 : 0;
+		if (matchingAxes < 2) {
+			// Cannot merge, return null to indicate this.
+			return null;
+		}
+		if (matchingAxes == 3) {
+			// These are the same zone; return self.
+			return this;
+		}
+
+		// Confirm zone fragments touch on mismatched axis
+		if (!xMatches) {
+			if (aMax.getX() != bMin.getX() && bMax.getX() != aMin.getX()) {
+				return null;
+			}
+		} else if (!yMatches) {
+			if (aMax.getY() != bMin.getY() && bMax.getY() != aMin.getY()) {
+				return null;
+			}
+		} else {
+			if (aMax.getZ() != bMin.getZ() && bMax.getZ() != aMin.getZ()) {
+				return null;
+			}
+		}
+
+		// Merging is possible, go for it.
+		ZoneFragment result;
+		try {
+			result = new ZoneFragment(this);
+		} catch (Exception e) {
+			throw e;
+		}
+
+		Vector resultMin = Vector.getMinimum(aMin, bMin);
+		Vector resultMax = Vector.getMaximum(aMax, bMax);
+		result.minCorner(resultMin);
+		result.maxCorner(resultMax);
+
+		return result;
+	}
+}
