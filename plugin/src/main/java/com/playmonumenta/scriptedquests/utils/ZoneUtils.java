f62ffff239ad4584820e527d9aa8356888ffb211
NickNackGus <NickNackGus@gmail.com>
2020-04-09 18:07:06 -0400

    Updated zone color code generation for better color differences

--- plugin/src/main/java/com/playmonumenta/scriptedquests/utils/ZoneUtils.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/utils/ZoneUtils.java
@@ -30,6 +30,10 @@ public class ZoneUtils {
 		}
 	}
 
+	public static float floatFloorMod(float x, float y) {
+		return x - ((float) Math.floor(x/y) * y);
+	}
+
 	public static int getColor(String layerName, String zoneName) {
 		int layerNameHash = layerName.hashCode();
 		int zoneNameHash = zoneName.hashCode();
@@ -40,7 +44,7 @@ public class ZoneUtils {
 		// Magic numbers can be tweaked for better visual effect.
 		// Bitwise & used in place of modulus, as % is remainder (breaks on negatives!)
 		// H; Default doesn't *need* to be blue, but I *like* it blue.
-		float hue = (float) layerNameHash / 1023.0f + 125.0f / 360.0f;
+		float hue = ZoneUtils.floatFloorMod((float) layerNameHash / 767.0f + 203.0f / 360.0f, 1.0f);
 		// Saturation and value (brightness) are biased away from gray.
 		// S
 		float saturation = 1.0f - 0.6f * ((float) (zoneNameHash & 0xffff)) / (float) 0xffff;
