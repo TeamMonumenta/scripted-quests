63568e63dc8bcce10379d1c15df72d539a5e93df
rayman520 <tenyman520@gmail.com>
2020-06-01 08:28:55 +0200

    Heal command: above max health fix

--- plugin/src/main/java/com/playmonumenta/scriptedquests/commands/Heal.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/commands/Heal.java
@@ -5,6 +5,9 @@ import io.github.jorelali.commandapi.api.CommandPermission;
 import io.github.jorelali.commandapi.api.arguments.Argument;
 import io.github.jorelali.commandapi.api.arguments.DoubleArgument;
 import io.github.jorelali.commandapi.api.arguments.EntitySelectorArgument;
+import org.bukkit.attribute.Attributable;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Damageable;
 
 import java.util.Collection;
@@ -24,7 +27,15 @@ public class Heal {
 				if (args[0] instanceof Collection<?>) {
 					for (Object e : (Collection<?>) args[0]) {
 						if (e instanceof Damageable) {
-							((Damageable) e).setHealth(((Damageable) e).getHealth() + (Double) args[1]);
+							Damageable d = (Damageable)e;
+							d.setHealth(d.getHealth() + (Double)args[1]);
+							if (e instanceof Attributable) {
+								Attributable a = (Attributable)e;
+								AttributeInstance attr = a.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+								if (attr != null && d.getHealth() > attr.getValue()) {
+									d.setHealth(attr.getValue());
+								}
+							}
 						}
 					}
 				}
