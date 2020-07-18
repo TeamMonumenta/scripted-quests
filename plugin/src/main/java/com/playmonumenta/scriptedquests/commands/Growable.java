16e9419792ec34035775775784a0e12452d32dab
Byron Marohn <combustible@live.com>
2020-07-18 06:37:03 +0000

    Working?
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/commands/Growable.java
@@ -0,0 +1,63 @@
+package com.playmonumenta.scriptedquests.commands;
+
+import java.util.LinkedHashMap;
+
+import com.playmonumenta.scriptedquests.managers.GrowableManager;
+
+import org.bukkit.Location;
+
+import io.github.jorelali.commandapi.api.CommandAPI;
+import io.github.jorelali.commandapi.api.CommandPermission;
+import io.github.jorelali.commandapi.api.arguments.Argument;
+import io.github.jorelali.commandapi.api.arguments.BooleanArgument;
+import io.github.jorelali.commandapi.api.arguments.DynamicSuggestedStringArgument;
+import io.github.jorelali.commandapi.api.arguments.IntegerArgument;
+import io.github.jorelali.commandapi.api.arguments.LiteralArgument;
+import io.github.jorelali.commandapi.api.arguments.LocationArgument;
+import io.github.jorelali.commandapi.api.arguments.StringArgument;
+
+public class Growable {
+	public static void register(GrowableManager manager) {
+		CommandPermission perm = CommandPermission.fromString("monumenta.growstructure");
+
+		LinkedHashMap<String, Argument> arguments = new LinkedHashMap<>();
+		arguments.put("grow", new LiteralArgument("grow"));
+		arguments.put("location", new LocationArgument());
+		arguments.put("label", new DynamicSuggestedStringArgument(() -> manager.getLabels()));
+		arguments.put("ticksPerStep", new IntegerArgument(1));
+		arguments.put("blocksPerStep", new IntegerArgument(1));
+		arguments.put("callStructureGrowEvent", new BooleanArgument());
+
+		CommandAPI.getInstance().register("growable",
+		                                  perm,
+		                                  arguments,
+		                                  (sender, args) -> {
+											  try {
+												  int loaded = manager.grow((String)args[1], (Location)args[0], (Integer)args[2], (Integer)args[3], (Boolean)args[4]);
+												  sender.sendMessage("Successfully grew '" + (String)args[1] + "' placing " + Integer.toString(loaded) + " blocks");
+											  } catch (Exception e) {
+												  CommandAPI.fail(e.getMessage());
+											  }
+		                                  }
+		);
+
+		arguments.clear();
+		arguments.put("add", new LiteralArgument("add"));
+		arguments.put("location", new LocationArgument());
+		arguments.put("label", new StringArgument());
+		arguments.put("maxDepth", new IntegerArgument(1));
+
+		CommandAPI.getInstance().register("growable",
+		                                  perm,
+		                                  arguments,
+		                                  (sender, args) -> {
+											  try {
+												  int size = manager.add((String)args[1], (Location)args[0], (Integer)args[2]);
+												  sender.sendMessage("Successfully saved '" + (String)args[1] + "' containing " + Integer.toString(size) + " blocks");
+											  } catch (Exception e) {
+												  CommandAPI.fail(e.getMessage());
+											  }
+		                                  }
+		);
+	}
+}
---
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/managers/GrowableManager.java
@@ -0,0 +1,59 @@
+package com.playmonumenta.scriptedquests.managers;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.GsonBuilder;
+import com.playmonumenta.scriptedquests.Plugin;
+import com.playmonumenta.scriptedquests.quests.GrowableStructure;
+import com.playmonumenta.scriptedquests.utils.FileUtils;
+import com.playmonumenta.scriptedquests.utils.QuestUtils;
+
+import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+
+public class GrowableManager {
+	private final Map<String, GrowableStructure> mGrowables = new HashMap<>();
+	private final Plugin mPlugin;
+
+	public GrowableManager(Plugin plugin) {
+		mPlugin = plugin;
+		reload(plugin, null);
+	}
+
+	/*
+	 * If sender is non-null, it will be sent debugging information
+	 */
+	public void reload(Plugin plugin, CommandSender sender) {
+		mGrowables.clear();
+		QuestUtils.loadScriptedQuests(plugin, "growables", sender, (object) -> {
+			GrowableStructure growable = new GrowableStructure(object);
+			mGrowables.put(growable.getLabel(), growable);
+			return growable.getLabel() + ":" + Integer.toString(growable.getSize());
+		});
+	}
+
+	public int grow(String label, Location origin, int ticksPerStep, int blocksPerStep, boolean callStructureGrowEvent) throws Exception {
+		GrowableStructure growable = mGrowables.get(label);
+		if (growable == null) {
+			throw new Exception("Growable '" + label + "' does not exist");
+		}
+
+		return growable.grow(mPlugin, origin, ticksPerStep, blocksPerStep, callStructureGrowEvent);
+	}
+
+	public int add(String label, Location origin, int maxDepth) throws Exception {
+		GrowableStructure growable = new GrowableStructure(mPlugin, origin, label, maxDepth);
+		mGrowables.put(label, growable);
+
+		String path = mPlugin.getDataFolder() + File.separator + "growables" + File.separator + label + ".json";
+		FileUtils.writeFile(path, new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create().toJson(growable.getAsJsonObject()));
+
+		return growable.getSize();
+	}
+
+	public String[] getLabels(){
+		return mGrowables.keySet().toArray(new String[mGrowables.size()]);
+	}
+}
