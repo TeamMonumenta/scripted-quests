ae9fd4d9b4036ea848c351d449a119ee17e9e3a1
ArnoGeukens <47953828+ArnoGeukens@users.noreply.github.com>
2020-09-03 20:51:08 +0200

    Fix /heal not working when healing more than max HP (#103)

--- plugin/src/main/java/com/playmonumenta/scriptedquests/commands/Heal.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/commands/Heal.java
@@ -1,17 +1,18 @@
 package com.playmonumenta.scriptedquests.commands;
 
-import io.github.jorelali.commandapi.api.CommandAPI;
-import io.github.jorelali.commandapi.api.CommandPermission;
-import io.github.jorelali.commandapi.api.arguments.Argument;
-import io.github.jorelali.commandapi.api.arguments.DoubleArgument;
-import io.github.jorelali.commandapi.api.arguments.EntitySelectorArgument;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+
 import org.bukkit.attribute.Attributable;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Damageable;
 
-import java.util.Collection;
-import java.util.LinkedHashMap;
+import io.github.jorelali.commandapi.api.CommandAPI;
+import io.github.jorelali.commandapi.api.CommandPermission;
+import io.github.jorelali.commandapi.api.arguments.Argument;
+import io.github.jorelali.commandapi.api.arguments.DoubleArgument;
+import io.github.jorelali.commandapi.api.arguments.EntitySelectorArgument;
 
 public class Heal {
 	public static void register() {
@@ -28,13 +29,16 @@ public class Heal {
 					for (Object e : (Collection<?>) args[0]) {
 						if (e instanceof Damageable) {
 							Damageable d = (Damageable)e;
-							d.setHealth(d.getHealth() + (Double)args[1]);
 							if (e instanceof Attributable) {
 								Attributable a = (Attributable)e;
 								AttributeInstance attr = a.getAttribute(Attribute.GENERIC_MAX_HEALTH);
-								if (attr != null && d.getHealth() > attr.getValue()) {
+								if (attr != null && (d.getHealth() + (Double)args[1]) > attr.getValue()) {
 									d.setHealth(attr.getValue());
+								} else {
+									d.setHealth(d.getHealth() + (Double)args[1]);
 								}
+							} else {
+								d.setHealth(d.getHealth() + (Double)args[1]);
 							}
 						}
 					}
