35ba304b2984cffcf0acd9ab7bba31dd0cb29ff0
Timothy Southwick <NickNackGus@gmail.com>
2020-09-26 01:48:43 -0400

    Removed unrequired import

--- plugin/src/main/java/com/playmonumenta/scriptedquests/utils/QuestUtils.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/utils/QuestUtils.java
@@ -14,8 +14,6 @@ import org.bukkit.plugin.Plugin;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
-import com.playmonumenta.scriptedquests.utils.MessagingUtils;
-
 public class QuestUtils {
 	@FunctionalInterface
 	public interface JsonLoadAction {
