923b221b87a0322ae78c23b2d0fc81a9098b7e09
Qoo <53715437+qooqoomagic@users.noreply.github.com>
2020-10-27 14:04:27 -0400

    Fix race NPE (#112)

--- plugin/src/main/java/com/playmonumenta/scriptedquests/Plugin.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/Plugin.java
@@ -136,7 +136,7 @@ public class Plugin extends JavaPlugin {
 		mTradeManager = new NpcTradeManager();
 		mLoginManager = new QuestLoginManager();
 		mDeathManager = new QuestDeathManager();
-		mRaceManager = new RaceManager();
+		mRaceManager = new RaceManager(this);
 		mCodeManager = new CodeManager();
 		mZoneManager = new ZoneManager(this);
 		mZonePropertyManager = new ZonePropertyManager(this);
