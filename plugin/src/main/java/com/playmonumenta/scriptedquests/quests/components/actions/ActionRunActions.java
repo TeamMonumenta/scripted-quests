9ea6dcb5aafaea1d4cb9ee4ed8f5f3d07b3f4811
WarlordWeaponry <minecraft2013@cox.net>
2020-12-28 01:46:57 -0600

    small absorption change

--- plugin/src/main/java/com/playmonumenta/scriptedquests/quests/components/actions/ActionRunActions.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/quests/components/actions/ActionRunActions.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.playmonumenta.scriptedquests.Plugin;
 import com.playmonumenta.scriptedquests.quests.components.QuestPrerequisites;
+import me.Novalescent.Core;
 import me.Novalescent.mobs.spells.scripted.actions.SpellActions;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -12,23 +13,29 @@ public class ActionRunActions implements ActionBase {
 
 	public boolean mOnPlayer;
 	public double mYOffset;
-	public SpellActions mActions;
+
+	public JsonElement mActionsJson;
 	public ActionRunActions(JsonElement value) throws Exception {
 		JsonObject json = value.getAsJsonObject();
 
 		mOnPlayer = json.get("on_player").getAsBoolean();
 		mYOffset = json.get("y_offset").getAsDouble();
 
-		mActions = new SpellActions(null, null, json.get("spell_actions"));
+		mActionsJson = json.get("spell_actions");
 	}
 	@Override
 	public void doAction(Plugin plugin, Player player, Entity npcEntity, QuestPrerequisites prereqs) {
-		if (mOnPlayer) {
-			mActions.doActions(player, player.getLocation().add(0, mYOffset, 0));
-		} else {
-			if (npcEntity != null) {
-				mActions.doActions(npcEntity, npcEntity.getLocation().add(0, mYOffset, 0));
+		try {
+			SpellActions actions = new SpellActions(Core.getInstance(), null, mActionsJson);
+			if (mOnPlayer) {
+				actions.doActions(player, player.getLocation().add(0, mYOffset, 0));
+			} else {
+				if (npcEntity != null) {
+					actions.doActions(npcEntity, npcEntity.getLocation().add(0, mYOffset, 0));
+				}
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
