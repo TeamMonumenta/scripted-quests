9a2e8623c5c379773dd802934c94274b4c27eb7c
Cloud <44526554+Cloud7050@users.noreply.github.com>
2021-02-12 23:19:26 +0800

    feat(DateUtils): Days since epoch refactored. Seconds since epoch's return value now matches the relative time value returned by days since epoch (and similar to the other methods' return values).
    This took me a surprisingly long time to think out and test while comparing the results to the previous implementation. So many lines of deleted comments

--- plugin/src/main/java/com/playmonumenta/scriptedquests/utils/DateUtils.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/utils/DateUtils.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
-import java.time.temporal.ChronoUnit;
 
 public class DateUtils {
 	// TODO Read from config, default to system time zone ala ZoneId.systemDefault()
@@ -24,15 +23,24 @@ public class DateUtils {
 
 	// 1 is Sunday, 7 is Saturday
 	public static int getDayOfWeek() {
+		// .getValue() gives 1 for Monday, 7 for Sunday, so we cycle the numbers
 		return LocalDateTime.now(mTz).getDayOfWeek().getValue() % 7 + 1;
 	}
 
 	public static long getDaysSinceEpoch() {
-		return ChronoUnit.DAYS.between(LocalDate.ofEpochDay(0), LocalDateTime.now(mTz));
+		// In our specified timezone, how many days we perceive it is since our 1 Jan 1970.
+		// Different timezones have different dates for the same point in time,
+		// so this simple comparison will yield different numbers of days for them
+		return LocalDate.now(mTz).toEpochDay();
 	}
 
 	public static long getSecondsSinceEpoch() {
-		return ChronoUnit.SECONDS.between(LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.UTC), LocalDateTime.now(ZoneOffset.UTC));
+		// In our specified timezone, how many seconds we perceive it is since our 1 Jan 1970.
+		// LocalDateTime "does not store or represent a time-zone",
+		// "It cannot represent an instant on the time-line".
+		// It does not actually compare to the fixed point in time, unix epoch.
+		// Different timezones will yield different seconds for the same point in time
+		return LocalDateTime.now(mTz).toEpochSecond(ZoneOffset.UTC); // No further offset applied to our number of seconds
 	}
 
 	public static int getAmPm() {
