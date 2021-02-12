c934ac68d5d8436b7577b15e45d9c03726cb9229
Cloud <44526554+Cloud7050@users.noreply.github.com>
2021-02-12 23:26:05 +0800

    feat(DateUtils): Sync up quest stuff with play's daily reboots

--- plugin/src/main/java/com/playmonumenta/scriptedquests/utils/DateUtils.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/utils/DateUtils.java
@@ -6,32 +6,37 @@ import java.time.ZoneId;
 import java.time.ZoneOffset;
 
 public class DateUtils {
-	// TODO Read from config, default to system time zone ala ZoneId.systemDefault()
-	public static ZoneId mTz = ZoneId.of("America/New_York");
+	//TODO Read from config, default to system time zone aka ZoneId.systemDefault() - Tim
+
+	// Offset server time to UTC-9 to change when the new day arrives.
+	// getDaysSinceEpoch() uses its own perceived epoch
+	// so it should sync up nicely with changes in getDayOfWeek().
+	// With this, quest stuff should match play's daily resets & daily reboots at 9am UTC
+	public static ZoneId TIMEZONE = ZoneOffset.of("-9");
 
 	public static int getYear() {
-		return LocalDateTime.now(mTz).getYear();
+		return LocalDateTime.now(TIMEZONE).getYear();
 	}
 
 	public static int getMonth() {
-		return LocalDateTime.now(mTz).getMonthValue();
+		return LocalDateTime.now(TIMEZONE).getMonthValue();
 	}
 
 	public static int getDayOfMonth() {
-		return LocalDateTime.now(mTz).getDayOfMonth();
+		return LocalDateTime.now(TIMEZONE).getDayOfMonth();
 	}
 
 	// 1 is Sunday, 7 is Saturday
 	public static int getDayOfWeek() {
 		// .getValue() gives 1 for Monday, 7 for Sunday, so we cycle the numbers
-		return LocalDateTime.now(mTz).getDayOfWeek().getValue() % 7 + 1;
+		return LocalDateTime.now(TIMEZONE).getDayOfWeek().getValue() % 7 + 1;
 	}
 
 	public static long getDaysSinceEpoch() {
 		// In our specified timezone, how many days we perceive it is since our 1 Jan 1970.
 		// Different timezones have different dates for the same point in time,
 		// so this simple comparison will yield different numbers of days for them
-		return LocalDate.now(mTz).toEpochDay();
+		return LocalDate.now(TIMEZONE).toEpochDay();
 	}
 
 	public static long getSecondsSinceEpoch() {
@@ -40,7 +45,7 @@ public class DateUtils {
 		// "It cannot represent an instant on the time-line".
 		// It does not actually compare to the fixed point in time, unix epoch.
 		// Different timezones will yield different seconds for the same point in time
-		return LocalDateTime.now(mTz).toEpochSecond(ZoneOffset.UTC); // No further offset applied to our number of seconds
+		return LocalDateTime.now(TIMEZONE).toEpochSecond(ZoneOffset.UTC); // No further offset applied to our number of seconds
 	}
 
 	public static int getAmPm() {
@@ -48,7 +53,7 @@ public class DateUtils {
 	}
 
 	public static int getHourOfDay() {
-		return LocalDateTime.now(mTz).getHour();
+		return LocalDateTime.now(TIMEZONE).getHour();
 	}
 
 	public static int getHourOfTwelve() {
@@ -57,14 +62,14 @@ public class DateUtils {
 	}
 
 	public static int getMinute() {
-		return LocalDateTime.now(mTz).getMinute();
+		return LocalDateTime.now(TIMEZONE).getMinute();
 	}
 
 	public static int getSecond() {
-		return LocalDateTime.now(mTz).getSecond();
+		return LocalDateTime.now(TIMEZONE).getSecond();
 	}
 
 	public static int getMs() {
-		return LocalDateTime.now(mTz).getNano() / 1000000;
+		return LocalDateTime.now(TIMEZONE).getNano() / 1000000;
 	}
 }
