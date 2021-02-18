216a21477e9d9bf15a0cb43f05f22758b1ebac0c
WarlordWeaponry <minecraft2013@cox.net>
2021-02-18 10:50:37 -0600

    Date replacement

--- plugin/src/main/java/com/playmonumenta/scriptedquests/scriptedtimer/Timer.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/scriptedtimer/Timer.java
@@ -1,4 +1,29 @@
 package com.playmonumenta.scriptedquests.scriptedtimer;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.time.DayOfWeek;
+
 public class Timer {
+
+	public final String mId;
+	public final DayOfWeek mDayOfWeek;
+	public final double mHours;
+	public Timer(JsonElement value) throws Exception {
+		JsonObject json = value.getAsJsonObject();
+		if (json == null) {
+			throw new Exception("Value for a timer is not a JsonObject!");
+		}
+
+		if (json.has("id")) {
+			mId = json.get("id").getAsString();
+		} else {
+			throw new Exception("JsonObject for a Timer does not have an id!");
+		}
+
+		mDayOfWeek = DayOfWeek.valueOf(json.get("dayOfTheWeek").getAsString());
+		mHours = json.get("resetHours").getAsDouble();
+	}
+
 }
