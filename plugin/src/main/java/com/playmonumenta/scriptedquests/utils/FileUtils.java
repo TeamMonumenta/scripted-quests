38e62da709caac200833b91464c0632240a09cca
BuildTools <unconfigured@null.spigotmc.org>
2021-04-24 21:15:57 +0200

    Translations V2.5 - realPath fix

--- plugin/src/main/java/com/playmonumenta/scriptedquests/utils/FileUtils.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/utils/FileUtils.java
@@ -54,7 +54,7 @@ public class FileUtils {
 
 	public static void writeFile(String fileName, String contents) throws IOException {
 		// Do not attempt to catch exceptions here - let them propagate to the caller
-		File file = new File(fileName); // Path.of() is somehow nonexistent?
+		File file = new File(fileName).toPath().toRealPath().toFile();
 
 		if (!file.exists()) {
 			file.getParentFile().mkdirs();
