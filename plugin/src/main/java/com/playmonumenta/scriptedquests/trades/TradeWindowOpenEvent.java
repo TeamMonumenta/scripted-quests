7f1ea45c7db7d11f6de8e7348c8f8e40e312a2d6
Byron Marohn <Combustible@live.com>
2021-11-27 19:44:50 +0000

    Remove obsolete comment

--- plugin/src/main/java/com/playmonumenta/scriptedquests/trades/TradeWindowOpenEvent.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/trades/TradeWindowOpenEvent.java
@@ -17,9 +17,6 @@ import com.playmonumenta.scriptedquests.quests.components.QuestActions;
  * This event is called when a player right-clicks a villager to start trading.
  * It contains all trades available to the player as per defined prerequisites.
  * The list of trades can be modified in this event and the changed trades will be displayed to the player instead, and the event can be cancelled completely to not open the trade window at all.
- * <p>
- * Note: if any trades have actions defined on them, their indices must not be changed in the trades list, or the actions will run for the wrong trades!
- * Preferably only add new trades or remove trades from the end.
  */
 public class TradeWindowOpenEvent extends Event implements Cancellable {
 
