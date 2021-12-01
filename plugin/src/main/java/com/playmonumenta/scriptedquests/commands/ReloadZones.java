986fba08a8ef4da2b0a539506942e3599cb5d26c
Timothy Southwick <NickNackGus@gmail.com>
2021-12-01 17:57:13 -0500

    Made /reloadzones a CommandAPI command

--- plugin/src/main/java/com/playmonumenta/scriptedquests/commands/ReloadZones.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/commands/ReloadZones.java
@@ -1,30 +1,22 @@
 package com.playmonumenta.scriptedquests.commands;
 
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+
+import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.CommandPermission;
 
 import com.playmonumenta.scriptedquests.Plugin;
 
-public class ReloadZones implements CommandExecutor {
-	Plugin mPlugin;
-
-	public ReloadZones(Plugin plugin) {
-		mPlugin = plugin;
-	}
-
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String arg2, String[] arg3) {
-		if (arg3.length > 0) {
-			sender.sendMessage(ChatColor.RED + "No parameters are needed for this function!");
-			return false;
-		}
-
-		sender.sendMessage(ChatColor.GOLD + "Reloading config...");
-
-		mPlugin.reloadZones(sender);
-
-		return true;
+public class ReloadZones {
+	public static void register(Plugin plugin) {
+		new CommandAPICommand("reloadzones")
+			.withPermission(CommandPermission.fromString("scriptedquests.reloadzones"))
+			.executes((sender, args) -> {
+				sender.sendMessage(ChatColor.GOLD + "Reloading config...");
+				plugin.reloadZones(sender);
+				return 1;
+			})
+			.register();
 	}
 }
