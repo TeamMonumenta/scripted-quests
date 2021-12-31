21d805e1e08461dd48937a66ada2b7278fc10a03
Peter Güttinger <njol@gmx.ch>
2021-12-31 04:40:51 +0100

    Added "/schedule command" (#170)

--- plugin/src/main/java/com/playmonumenta/scriptedquests/commands/ScheduleFunction.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/commands/ScheduleFunction.java
@@ -1,28 +1,42 @@
 package com.playmonumenta.scriptedquests.commands;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-
-import com.playmonumenta.scriptedquests.Plugin;
+import java.util.stream.Collectors;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ProxiedCommandSender;
+import org.bukkit.entity.Entity;
+
+import com.playmonumenta.scriptedquests.Plugin;
+import com.playmonumenta.scriptedquests.utils.CommandArgument;
 
 import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.CommandPermission;
 import dev.jorel.commandapi.arguments.FunctionArgument;
 import dev.jorel.commandapi.arguments.IntegerArgument;
-import dev.jorel.commandapi.arguments.LiteralArgument;
 import dev.jorel.commandapi.wrappers.FunctionWrapper;
 
 public class ScheduleFunction {
-	private class DelayedFunction {
-		protected int mTicksLeft;
-		protected FunctionWrapper[] mFunction;
+	private abstract class DelayedAction {
+		private int mTicksLeft;
 
-		protected DelayedFunction(int ticksLeftIn, FunctionWrapper[] functionIn) {
+		private DelayedAction(int ticksLeftIn) {
 			mTicksLeft = ticksLeftIn;
+		}
+
+		protected abstract void run();
+	}
+
+	private class DelayedFunction extends DelayedAction {
+		private final FunctionWrapper[] mFunction;
+
+		private DelayedFunction(int ticksLeftIn, FunctionWrapper[] functionIn) {
+			super(ticksLeftIn);
 			mFunction = functionIn;
 		}
 
@@ -31,38 +45,70 @@ public class ScheduleFunction {
 				func.run();
 			}
 		}
+
+		@Override
+		public String toString() {
+			return "DelayedFunction{" +
+				       "mFunction=" + Arrays.stream(mFunction).map(f -> f.getKey().toString()).collect(Collectors.joining(", ")) +
+				       '}';
+		}
 	}
 
-	private final List<DelayedFunction> mFunctions = new ArrayList<DelayedFunction>();
+	private class DelayedCommand extends DelayedAction {
+		private final CommandSender mSender;
+		private final String mCommand;
+
+		private DelayedCommand(int ticksLeftIn, CommandSender sender, String command) {
+			super(ticksLeftIn);
+			mSender = sender;
+			mCommand = command;
+		}
+
+		protected void run() {
+			CommandSender sender = mSender;
+			if (sender instanceof ProxiedCommandSender) {
+				sender = ((ProxiedCommandSender) sender).getCallee();
+			}
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), sender instanceof Entity ? "execute as " + ((Entity) sender).getUniqueId() + " at @s run " + mCommand : mCommand);
+		}
+
+		@Override
+		public String toString() {
+			return "DelayedCommand{" +
+				       "mSender=" + mSender +
+				       ", mCommand='" + mCommand + '\'' +
+				       '}';
+		}
+	}
+
+	private final List<DelayedAction> mActions = new ArrayList<>();
 	private final Plugin mPlugin;
 	private Integer mTaskId = null;
 	private final Runnable mRunnable = new Runnable() {
 		// Re-use the same temporary list each iteration
-		private final List<DelayedFunction> mFunctionsToRun = new ArrayList<DelayedFunction>();
+		private final List<DelayedAction> mActionsToRun = new ArrayList<>();
 
 		@Override
 		public void run() {
-			Iterator<DelayedFunction> it = mFunctions.iterator();
+			Iterator<DelayedAction> it = mActions.iterator();
 			while (it.hasNext()) {
-				DelayedFunction entry = it.next();
+				DelayedAction entry = it.next();
 				entry.mTicksLeft--;
 
 				if (entry.mTicksLeft < 0) {
-					mFunctionsToRun.add(entry);
+					mActionsToRun.add(entry);
 
-					for (FunctionWrapper func : entry.mFunction) {
-						mPlugin.getLogger().fine("Preparing to run " + func.getKey().toString());
-					}
+					mPlugin.getLogger().fine("Preparing to run " + entry);
 					it.remove();
 				}
 			}
 
-			for (DelayedFunction entry : mFunctionsToRun) {
+			for (DelayedAction entry : mActionsToRun) {
 				entry.run();
 			}
-			mFunctionsToRun.clear();
+			mActionsToRun.clear();
 
-			if (mFunctions.isEmpty()) {
+			if (mActions.isEmpty()) {
 				Bukkit.getScheduler().cancelTask(mTaskId);
 				mTaskId = null;
 			}
@@ -82,19 +128,35 @@ public class ScheduleFunction {
 
 		new CommandAPICommand("schedule")
 			.withPermission(CommandPermission.fromString("scriptedquests.schedulefunction"))
-			.withArguments(new LiteralArgument("function").setListed(false))
-			.withArguments(new FunctionArgument("function"))
-			.withArguments(new IntegerArgument("ticks", 0))
-			.executes((sender, args) -> {
-				addDelayedFunction((FunctionWrapper[])args[0], (Integer)args[1]);
-			})
+			.withSubcommand(
+				new CommandAPICommand("function")
+					.withArguments(new FunctionArgument("function"),
+					               new IntegerArgument("ticks", 0))
+					.executes((sender, args) -> {
+						addDelayedFunction((FunctionWrapper[]) args[0], (Integer) args[1]);
+					}))
+			.withSubcommand(
+				new CommandAPICommand("command")
+					.withArguments(new IntegerArgument("ticks", 0),
+					               new CommandArgument("cmd"))
+					.executes((sender, args) -> {
+						addDelayedCommand(sender, (String) args[1], (Integer) args[0]);
+					}))
 			.register();
 
 		/* TODO: Add the other /schedule variants (clear and replace/append variants) */
 	}
 
 	private void addDelayedFunction(FunctionWrapper[] function, int ticks) {
-		mFunctions.add(new DelayedFunction(ticks, function));
+		mActions.add(new DelayedFunction(ticks, function));
+
+		if (mTaskId == null) {
+			mTaskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(mPlugin, mRunnable, 0, 1);
+		}
+	}
+
+	private void addDelayedCommand(CommandSender sender, String command, int ticks) {
+		mActions.add(new DelayedCommand(ticks, sender, command));
 
 		if (mTaskId == null) {
 			mTaskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(mPlugin, mRunnable, 0, 1);
@@ -103,11 +165,11 @@ public class ScheduleFunction {
 
 	/* Run all the remaining commands now, even though they are scheduled for later */
 	public void cancel() {
-		for (DelayedFunction entry : new ArrayList<>(mFunctions)) {
+		for (DelayedAction entry : new ArrayList<>(mActions)) {
 			// Note that these functions might add more functions... but there's nothing we can do about that
 			entry.run();
 		}
-		mFunctions.clear();
+		mActions.clear();
 
 		if (mTaskId != null) {
 			Bukkit.getScheduler().cancelTask(mTaskId);
---
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/utils/CommandArgument.java
@@ -0,0 +1,59 @@
+package com.playmonumenta.scriptedquests.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.util.StringUtil;
+
+import dev.jorel.commandapi.SuggestionInfo;
+import dev.jorel.commandapi.arguments.GreedyStringArgument;
+
+public class CommandArgument extends GreedyStringArgument {
+
+	public CommandArgument(String nodeName) {
+		super(nodeName);
+		replaceSuggestions(this::suggestions);
+	}
+
+	private String[] suggestions(SuggestionInfo info) {
+		String input = info.currentArg();
+		CommandSender sender = info.sender();
+		String[] cmd = input.split(" ", Integer.MAX_VALUE);
+		if (cmd.length == 1) {
+			String prefix = cmd[0];
+			ArrayList<String> completions = new ArrayList<>();
+			for (Map.Entry<String, Command> knownCommand : Bukkit.getCommandMap().getKnownCommands().entrySet()) {
+				if (knownCommand.getValue().testPermissionSilent(sender)) {
+					String name = knownCommand.getKey();
+					if (StringUtil.startsWithIgnoreCase(name, prefix)) {
+						completions.add(name);
+					}
+				}
+			}
+			completions.sort(String.CASE_INSENSITIVE_ORDER);
+			return completions.toArray(new String[0]);
+		} else {
+			Command command = Bukkit.getCommandMap().getCommand(cmd[0]);
+			if (command == null || !command.testPermissionSilent(sender)) {
+				return new String[0];
+			}
+			List<String> tabComplete = command.tabComplete(sender, cmd[0], Arrays.copyOfRange(cmd, 1, cmd.length), null);
+			String lastArg = cmd[cmd.length - 1];
+			if (tabComplete.stream().allMatch(tc -> StringUtils.startsWithIgnoreCase(tc, lastArg))) {
+				String allButLastArg = input.substring(0, input.lastIndexOf(' '));
+				return tabComplete.stream().map(s -> allButLastArg + ' ' + s).toArray(String[]::new);
+			} else {
+				// Bukkit's tab completion API is missing the "range" of a suggestion, so we don't know which parts to overwrite.
+				// Just add it at the end, and the user will have to correct it if it's wrong.
+				return tabComplete.stream().map(s -> input + s).toArray(String[]::new);
+			}
+		}
+	}
+
+}
