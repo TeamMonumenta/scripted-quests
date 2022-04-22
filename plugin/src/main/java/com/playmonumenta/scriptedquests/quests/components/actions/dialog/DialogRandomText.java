9a9961332ef66358aa39fe091f2e56248177c7ab
Peter Güttinger <njol@gmx.ch>
2022-04-22 08:29:04 +0200

    Fixed null displayName breaking dialogues

--- plugin/src/main/java/com/playmonumenta/scriptedquests/quests/components/actions/dialog/DialogRandomText.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/quests/components/actions/dialog/DialogRandomText.java
@@ -5,23 +5,22 @@ import com.playmonumenta.scriptedquests.api.JsonObjectBuilder;
 import com.playmonumenta.scriptedquests.quests.QuestContext;
 import com.playmonumenta.scriptedquests.utils.MessagingUtils;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Random;
+import org.jetbrains.annotations.Nullable;
 
 public class DialogRandomText implements DialogBase {
-	private String mDisplayName;
-	private ArrayList<String> mText = new ArrayList<String>();
-	private Random mRandom = new Random();
+	private final @Nullable String mDisplayName;
+	private final ArrayList<String> mText = new ArrayList<>();
+	private final Random mRandom = new Random();
 
-	public DialogRandomText(String displayName, JsonElement element) throws Exception {
+	public DialogRandomText(@Nullable String displayName, JsonElement element) throws Exception {
 		mDisplayName = displayName;
 
 		if (element.isJsonPrimitive()) {
 			mText.add(element.getAsString());
 		} else if (element.isJsonArray()) {
-			Iterator<JsonElement> iter = element.getAsJsonArray().iterator();
-			while (iter.hasNext()) {
-				mText.add(iter.next().getAsString());
+			for (JsonElement jsonElement : element.getAsJsonArray()) {
+				mText.add(jsonElement.getAsString());
 			}
 		} else {
 			throw new Exception("random_text value is neither an array nor a string!");
@@ -31,7 +30,11 @@ public class DialogRandomText implements DialogBase {
 	@Override
 	public void sendDialog(QuestContext context) {
 		int idx = mRandom.nextInt(mText.size());
-		MessagingUtils.sendNPCMessage(context.getPlayer(), mDisplayName, mText.get(idx));
+		if (mDisplayName != null && !mDisplayName.isEmpty()) {
+			MessagingUtils.sendNPCMessage(context.getPlayer(), mDisplayName, mText.get(idx));
+		} else {
+			MessagingUtils.sendRawMessage(context.getPlayer(), mText.get(idx));
+		}
 	}
 
 	@Override
