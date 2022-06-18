0fbcb6c12ecbfcfe634eb299f4445341d9cd8fd8
Timothy Southwick <NickNackGus@gmail.com>
2022-06-18 01:42:43 -0400

    Added vanilla loot table argument alternatives (#213)

--- plugin/src/main/java/com/playmonumenta/scriptedquests/commands/GiveLootTable.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/commands/GiveLootTable.java
@@ -6,6 +6,7 @@ import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.CommandPermission;
 import dev.jorel.commandapi.arguments.EntitySelectorArgument;
 import dev.jorel.commandapi.arguments.IntegerArgument;
+import dev.jorel.commandapi.arguments.LootTableArgument;
 import dev.jorel.commandapi.arguments.TextArgument;
 import java.util.Collection;
 import java.util.Random;
@@ -13,6 +14,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
+import org.bukkit.loot.LootTable;
 
 public class GiveLootTable {
 	@SuppressWarnings("unchecked")
@@ -22,9 +24,28 @@ public class GiveLootTable {
 		new CommandAPICommand("giveloottable")
 			.withPermission(perms)
 			.withArguments(new EntitySelectorArgument("players", EntitySelectorArgument.EntitySelector.MANY_PLAYERS))
+			.withArguments(new LootTableArgument("loot_table"))
+			.executes((sender, args) -> {
+				giveLoot((Collection<Player>)args[0], (LootTable)args[1], 1, random);
+			})
+			.register();
+
+		new CommandAPICommand("giveloottable")
+			.withPermission(perms)
+			.withArguments(new EntitySelectorArgument("players", EntitySelectorArgument.EntitySelector.MANY_PLAYERS))
 			.withArguments(new TextArgument("lootTablePath"))
 			.executes((sender, args) -> {
 				giveLoot((Collection<Player>)args[0], (String)args[1], 1, random);
+			})
+			.register();
+
+		new CommandAPICommand("giveloottable")
+			.withPermission(perms)
+			.withArguments(new EntitySelectorArgument("players", EntitySelectorArgument.EntitySelector.MANY_PLAYERS))
+			.withArguments(new LootTableArgument("loot_table"))
+			.withArguments(new IntegerArgument("count", 1, 1000))
+			.executes((sender, args) -> {
+				giveLoot((Collection<Player>)args[0], (LootTable)args[1], (Integer)args[2], random);
 			})
 			.register();
 
@@ -42,6 +63,16 @@ public class GiveLootTable {
 		new CommandAPICommand("giveloottable")
 			.withPermission(perms)
 			.withArguments(new EntitySelectorArgument("players", EntitySelectorArgument.EntitySelector.MANY_PLAYERS))
+			.withArguments(new LootTableArgument("loot_table"))
+			.withArguments(new EntitySelectorArgument("countItems", EntitySelectorArgument.EntitySelector.ONE_ENTITY))
+			.executes((sender, args) -> {
+				giveLootCountFromItemEntity((Collection<Player>)args[0], (LootTable)args[1], (Entity)args[2], random);
+			})
+			.register();
+
+		new CommandAPICommand("giveloottable")
+			.withPermission(perms)
+			.withArguments(new EntitySelectorArgument("players", EntitySelectorArgument.EntitySelector.MANY_PLAYERS))
 			.withArguments(new TextArgument("lootTablePath"))
 			.withArguments(new EntitySelectorArgument("countItems", EntitySelectorArgument.EntitySelector.ONE_ENTITY))
 			.executes((sender, args) -> {
@@ -51,8 +82,8 @@ public class GiveLootTable {
 	}
 
 	private static void giveLootCountFromItemEntity(Collection<Player> players, String lootPath, Entity entity, Random random) {
-		if ((entity instanceof Item) && ((Item)entity).getItemStack() != null) {
-			giveLoot(players, lootPath, ((Item)entity).getItemStack().getAmount(), random);
+		if (entity instanceof Item itemEntity) {
+			giveLoot(players, lootPath, itemEntity.getItemStack().getAmount(), random);
 		} else {
 			for (Player player : players) {
 				player.sendMessage(ChatColor.RED + "BUG! Server tried to give you loot based on an entity that wasn't an item from the table '" + lootPath + "'");
@@ -61,15 +92,41 @@ public class GiveLootTable {
 		}
 	}
 
+	private static void giveLootCountFromItemEntity(Collection<Player> players, LootTable lootTable, Entity entity, Random random) {
+		if (entity instanceof Item itemEntity) {
+			giveLoot(players, lootTable, itemEntity.getItemStack().getAmount(), random);
+		} else {
+			for (Player player : players) {
+				player.sendMessage(ChatColor.RED + "BUG! Server tried to give you loot based on an entity that wasn't an item from the table '" + lootTable.getKey() + "'");
+				player.sendMessage(ChatColor.RED + "Please take a screenshot and report this bug");
+			}
+		}
+	}
+
 	private static void giveLoot(Collection<Player> players, String lootPath, int count, Random random) {
-		boolean alreadyDone = false;
+		LootTable lootTable;
+		try {
+			lootTable = InventoryUtils.getLootTable(lootPath);
+		} catch (Exception e) {
+			for (Player player : players) {
+				player.sendMessage(ChatColor.RED + "BUG! Server failed to give you loot from the table '" + lootPath + "'");
+				player.sendMessage(ChatColor.RED + "Please hover over the following message, take a screenshot, and report this to a moderator");
+				MessagingUtils.sendStackTrace(player, e);
+			}
+			return;
+		}
+		giveLoot(players, lootTable, count, random);
+	}
+
+	private static void giveLoot(Collection<Player> players, LootTable lootTable, int count, Random random) {
 		for (Player player : players) {
+			boolean alreadyDone = false;
 			try {
 				for (int i = 0; i < count; i++) {
-					alreadyDone = InventoryUtils.giveLootTableContents(player, lootPath, random, alreadyDone);
+					alreadyDone = InventoryUtils.giveLootTableContents(player, lootTable, random, alreadyDone);
 				}
 			} catch (Exception e) {
-				player.sendMessage(ChatColor.RED + "BUG! Server failed to give you loot from the table '" + lootPath + "'");
+				player.sendMessage(ChatColor.RED + "BUG! Server failed to give you loot from the table '" + lootTable.getKey() + "'");
 				player.sendMessage(ChatColor.RED + "Please hover over the following message, take a screenshot, and report this to a moderator");
 				MessagingUtils.sendStackTrace(player, e);
 			}
