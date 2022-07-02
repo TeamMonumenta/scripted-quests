671bda20c377db2c3bbd9b4b4cb1d513858f73d2
Peter Güttinger <njol@gmx.ch>
2022-07-02 21:24:42 +0200

    Added keep_xp option to death files

--- plugin/src/main/java/com/playmonumenta/scriptedquests/quests/QuestDeath.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/quests/QuestDeath.java
@@ -7,6 +7,7 @@ import com.playmonumenta.scriptedquests.Plugin;
 import com.playmonumenta.scriptedquests.point.Point;
 import com.playmonumenta.scriptedquests.quests.components.QuestActions;
 import com.playmonumenta.scriptedquests.quests.components.QuestPrerequisites;
+import com.playmonumenta.scriptedquests.utils.ExperienceUtils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
@@ -38,6 +39,7 @@ public class QuestDeath {
 
 	private @Nullable Point mRespawnPt = null;
 	private boolean mKeepInv = false;
+	private Double mKeepXp = null;
 	private @Nullable QuestPrerequisites mPrerequisites = null;
 	private @Nullable QuestActions mActions = null;
 
@@ -48,48 +50,51 @@ public class QuestDeath {
 			JsonElement value = ent.getValue();
 
 			switch (key) {
-			case "keep_inventory":
-				mKeepInv = value.getAsBoolean();
-				break;
-			case "respawn_location":
-				JsonObject coordObj = value.getAsJsonObject();
-				// Read x coordinate
-				JsonElement xElement = coordObj.get("x");
-				if (xElement == null) {
-					throw new Exception("Failed to parse location x value!");
-				}
-				double x = xElement.getAsDouble();
+				case "keep_inventory":
+					mKeepInv = value.getAsBoolean();
+					break;
+				case "keep_xp":
+					mKeepXp = value.getAsDouble();
+					break;
+				case "respawn_location":
+					JsonObject coordObj = value.getAsJsonObject();
+					// Read x coordinate
+					JsonElement xElement = coordObj.get("x");
+					if (xElement == null) {
+						throw new Exception("Failed to parse location x value!");
+					}
+					double x = xElement.getAsDouble();
 
-				// Read y coordinate
-				JsonElement yElement = coordObj.get("y");
-				if (yElement == null) {
-					throw new Exception("Failed to parse location y value!");
-				}
-				double y = yElement.getAsDouble();
+					// Read y coordinate
+					JsonElement yElement = coordObj.get("y");
+					if (yElement == null) {
+						throw new Exception("Failed to parse location y value!");
+					}
+					double y = yElement.getAsDouble();
 
-				// Read z coordinate
-				JsonElement zElement = coordObj.get("z");
-				if (zElement == null) {
-					throw new Exception("Failed to parse location z value!");
-				}
-				double z = zElement.getAsDouble();
+					// Read z coordinate
+					JsonElement zElement = coordObj.get("z");
+					if (zElement == null) {
+						throw new Exception("Failed to parse location z value!");
+					}
+					double z = zElement.getAsDouble();
 
-				mRespawnPt = new Point(x, y, z);
+					mRespawnPt = new Point(x, y, z);
 
-				break;
-			case "prerequisites":
-				mPrerequisites = new QuestPrerequisites(value);
-				break;
-			case "actions":
-				mActions = new QuestActions("", "", EntityType.VILLAGER, 0, value);
-				break;
-			default:
-				throw new Exception("Unknown death quest key: '" + key + "'");
+					break;
+				case "prerequisites":
+					mPrerequisites = new QuestPrerequisites(value);
+					break;
+				case "actions":
+					mActions = new QuestActions("", "", EntityType.VILLAGER, 0, value);
+					break;
+				default:
+					throw new Exception("Unknown death quest key: '" + key + "'");
 			}
 		}
 
-		if (mRespawnPt == null && mKeepInv == false && mActions == null) {
-			throw new Exception("Death quest detected with no observeable effects!");
+		if (mRespawnPt == null && mKeepInv == false && mActions == null && mKeepXp == null) {
+			throw new Exception("Death quest detected with no observable effects!");
 		}
 	}
 
@@ -98,23 +103,40 @@ public class QuestDeath {
 	public boolean deathEvent(Plugin plugin, PlayerDeathEvent event) {
 		Player player = event.getEntity();
 		if (mPrerequisites == null || mPrerequisites.prerequisiteMet(new QuestContext(plugin, player, null))) {
-			List<DeathActions> actionsList;
-			if (player.hasMetadata(Constants.PLAYER_RESPAWN_ACTIONS_METAKEY)) {
-				actionsList = (List<DeathActions>) player.getMetadata(Constants.PLAYER_RESPAWN_ACTIONS_METAKEY).get(0).value();
-			} else {
-				actionsList = new ArrayList<DeathActions>(5);
-			}
 			if (mActions != null) {
+				List<DeathActions> actionsList;
+				if (player.hasMetadata(Constants.PLAYER_RESPAWN_ACTIONS_METAKEY)) {
+					actionsList = (List<DeathActions>) player.getMetadata(Constants.PLAYER_RESPAWN_ACTIONS_METAKEY).get(0).value();
+				} else {
+					actionsList = new ArrayList<>(5);
+				}
 				actionsList.add(new DeathActions(mActions, mPrerequisites));
+				player.setMetadata(Constants.PLAYER_RESPAWN_ACTIONS_METAKEY,
+					new FixedMetadataValue(plugin, actionsList));
 			}
-			player.setMetadata(Constants.PLAYER_RESPAWN_ACTIONS_METAKEY,
-				new FixedMetadataValue(plugin, actionsList));
 
 			event.setKeepInventory(mKeepInv);
 			event.setKeepLevel(mKeepInv);
 			if (mKeepInv) {
 				event.setDroppedExp(0);
 				event.getDrops().clear();
+			}
+			if (mKeepXp != null) {
+				event.setDroppedExp(0);
+				if (mKeepXp >= 1) {
+					event.setKeepLevel(true);
+				} else if (mKeepXp <= 0) {
+					event.setKeepLevel(false);
+					event.setNewLevel(0);
+					event.setNewExp(0);
+				} else {
+					event.setKeepLevel(false);
+					int newTotalXp = (int) Math.round(ExperienceUtils.getTotalExperience(player) * mKeepXp);
+					int newLevel = ExperienceUtils.getLevel(newTotalXp);
+					int newXp = newTotalXp - ExperienceUtils.getTotalExperience(newLevel);
+					event.setNewLevel(newLevel);
+					event.setNewExp(newXp);
+				}
 			}
 
 			if (mRespawnPt != null) {
@@ -123,7 +145,7 @@ public class QuestDeath {
 				 * they can be sent there
 				 */
 				player.setMetadata(Constants.PLAYER_RESPAWN_POINT_METAKEY,
-				                   new FixedMetadataValue(plugin, mRespawnPt));
+					new FixedMetadataValue(plugin, mRespawnPt));
 			}
 			return true;
 		}
---
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/utils/ExperienceUtils.java
@@ -0,0 +1,62 @@
+package com.playmonumenta.scriptedquests.utils;
+
+import org.bukkit.entity.Player;
+
+public class ExperienceUtils {
+
+	public static final int LEVEL_30 = 1395;
+	public static final int LEVEL_40 = 2920;
+	public static final int LEVEL_50 = 5345;
+	public static final int LEVEL_60 = 8670;
+	public static final int LEVEL_70 = 12895;
+	public static final int LEVEL_80 = 18020;
+
+	public static int getTotalExperience(int level) {
+		int xp = 0;
+
+		if (level >= 0 && level <= 15) {
+			xp = (int) Math.round(Math.pow(level, 2) + 6 * level);
+		} else if (level > 15 && level <= 30) {
+			xp = (int) Math.round(2.5 * Math.pow(level, 2) - 40.5 * level + 360);
+		} else if (level > 30) {
+			xp = (int) Math.round(4.5 * Math.pow(level, 2) - 162.5 * level + 2220);
+		}
+		return xp;
+	}
+
+	public static int getTotalExperience(Player player) {
+		return Math.round(player.getExp() * player.getExpToLevel()) + getTotalExperience(player.getLevel());
+	}
+
+	public static int getLevel(int totalXp) {
+		float a = 0;
+		float b = 0;
+		float c = -totalXp;
+
+		if (totalXp > getTotalExperience(0) && totalXp <= getTotalExperience(15)) {
+			a = 1;
+			b = 6;
+		} else if (totalXp > getTotalExperience(15) && totalXp <= getTotalExperience(30)) {
+			a = 2.5f;
+			b = -40.5f;
+			c += 360;
+		} else if (totalXp > getTotalExperience(30)) {
+			a = 4.5f;
+			b = -162.5f;
+			c += 2220;
+		}
+		return (int) Math.floor((-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a));
+	}
+
+	public static void setTotalExperience(Player player, int amount) {
+		int level = getLevel(amount);
+		int xp = amount - getTotalExperience(level);
+		player.setLevel(level);
+		player.setExp(0);
+		player.giveExp(xp);
+	}
+
+	public static void addTotalExperience(Player player, int amount) {
+		setTotalExperience(player, amount + getTotalExperience(player));
+	}
+}
