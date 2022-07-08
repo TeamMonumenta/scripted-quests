80136e58f4c59adb8d32b7d08dc7351c7fed7393
Peter Güttinger <njol@gmx.ch>
2022-07-08 15:00:05 +0200

    Fixed interactibles triggering on all types of damage instead of only melee

--- plugin/src/main/java/com/playmonumenta/scriptedquests/listeners/InteractablesListener.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/listeners/InteractablesListener.java
@@ -14,6 +14,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerAnimationEvent;
@@ -107,13 +108,11 @@ public class InteractablesListener implements Listener {
 		Entity damagee = event.getEntity();
 		Entity damager = event.getDamager();
 
-		if (damager instanceof Player) {
-			Player player = (Player)damager;
-			ItemStack item = player.getInventory().getItemInMainHand();
-			if (player.isRiptiding()) {
-				return;
-			}
+		if (damager instanceof Player player
+			    && event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK
+			    && !player.isRiptiding()) {
 
+			ItemStack item = player.getInventory().getItemInMainHand();
 			if (item != null && !item.getType().isAir() && player.getCooldown(item.getType()) > 0) {
 				/* Player's item is on cooldown, don't use it to interact to trigger interactions */
 				return;
