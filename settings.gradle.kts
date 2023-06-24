67a85a209aa8328ccac12e359221ba41915153a5
usb <56985400+U5B@users.noreply.github.com>
2023-06-24 12:29:41 -0700

    Add 1.19.4 version adapter (#273)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "scripted-quests"
 include(":adapter_api")
 include(":adapter_unsupported")
 include(":adapter_v1_18_R2")
+include(":adapter_v1_19_R3")
 include(":scripted-quests")
 project(":scripted-quests").projectDir = file("plugin")
 
