000bfe10256002f1f2a474961af91bb18ec2de32
Timothy Southwick <NickNackGus@gmail.com>
2023-10-08 16:49:20 -0400

    Added volt-deploy

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -165,6 +165,19 @@ tasks.create("stage-deploy") {
     }
 }
 
+tasks.create("volt-deploy") {
+    val shadowJar by tasks.named<ShadowJar>("shadowJar")
+    dependsOn(shadowJar)
+    doLast {
+        ssh.runSessions {
+            session(basicssh) {
+                put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/volt/m12/server_config/plugins")
+                execute("cd /home/epic/volt/m12/server_config/plugins && rm -f ScriptedQuests.jar && ln -s " + shadowJar.archiveFileName.get() + " ScriptedQuests.jar")
+            }
+        }
+    }
+}
+
 tasks.create("build-deploy") {
     val shadowJar by tasks.named<ShadowJar>("shadowJar")
     dependsOn(shadowJar)
