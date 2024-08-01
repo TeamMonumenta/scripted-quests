dee06fe957a277b0714f3ef47fe2b28074a8a149
Floweynt <guywithanaxe42@gmail.com>
2024-08-01 16:38:38 -0500

    fix dep stuff

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
 	testRuntimeOnly("dev.jorel:commandapi-bukkit-core:9.4.0")
 	errorprone("com.google.errorprone:error_prone_core:2.29.1")
 	errorprone("com.uber.nullaway:nullaway:0.9.5")
-	compileOnly("com.playmonumenta:redissync:4.5")
+	compileOnly("com.playmonumenta:redissync:4.6.1:all")
 }
 configurations {
 	testCompileOnly.get().extendsFrom(compileOnly.get())
