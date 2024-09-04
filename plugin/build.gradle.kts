adbfd728c132b35eb3acc126240ff39e729be5e4
Floweynt <guywithanaxe42@gmail.com>
2024-09-04 16:28:04 -0500

    move to libs.versions.toml

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -3,21 +3,25 @@ repositories {
 }
 
 dependencies {
-	implementation("com.google.api-client:google-api-client:1.31.4")
-	implementation("com.google.apis:google-api-services-sheets:v4-rev1-1.21.0")
-	implementation("com.google.auth:google-auth-library-oauth2-http:0.1.0")
-	compileOnly("dev.jorel:commandapi-bukkit-core:9.4.1")
-	compileOnly("de.tr7zw:item-nbt-api-plugin:2.12.0-SNAPSHOT")
-	compileOnly("com.mojang:brigadier:1.0.17")
-	compileOnly("com.google.code.gson:gson:2.8.0")
-	compileOnly("org.dynmap:DynmapCoreAPI:2.0")
-	compileOnly("com.comphenix.protocol:ProtocolLib:4.7.0")
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-	testImplementation("org.mockito:mockito-junit-jupiter:5.8.0")
-	testImplementation("com.github.seeseemelk:MockBukkit-v1.20:3.9.0")
-	testRuntimeOnly("dev.jorel:commandapi-bukkit-core:9.4.0")
-	compileOnly("com.playmonumenta:redissync:4.6.1:all")
+	implementation(libs.bundles.google.api)
+
+	compileOnly(libs.commandapi)
+	compileOnly(libs.nbtapi)
+	compileOnly(libs.brigadier)
+	compileOnly(libs.gson)
+	compileOnly(libs.dynmap)
+	compileOnly(libs.protocollib)
+	compileOnly(libs.redissync) {
+		artifact {
+			classifier = "all"
+		}
+	}
+
+	testImplementation(libs.jupiter.api)
+	testImplementation(libs.mockito)
+	testImplementation(libs.mockbukkit)
+	testRuntimeOnly(libs.jupiter.engine)
+	testRuntimeOnly(libs.commandapi)
 }
 
 configurations {
