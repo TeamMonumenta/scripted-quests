35a002d6e3755b950f719af3a70a33cd0148a211
Byron Marohn <combustible@live.com>
2026-04-08 16:31:28 +0000

    Fix lost color in race text (#343)
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- plugin/src/main/java/com/playmonumenta/scriptedquests/races/RaceTime.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/races/RaceTime.java
@@ -4,14 +4,18 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.playmonumenta.scriptedquests.quests.QuestContext;
 import com.playmonumenta.scriptedquests.quests.components.QuestActions;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.jetbrains.annotations.Nullable;
 
 public class RaceTime implements Comparable<RaceTime> {
 	private final String mLabel;
 	protected final int mTime;
-	private final String mColor;
+	private final NamedTextColor mTextColor;
+	private final boolean mBold;
 	private final @Nullable QuestActions mActions;
 
 	public RaceTime(JsonElement element) throws Exception {
@@ -38,11 +42,13 @@ public class RaceTime implements Comparable<RaceTime> {
 		}
 		mTime = (int)(timeInSeconds * 1000);
 
-
 		// color
 		JsonElement colorElement = object.get("color");
 		if (colorElement != null) {
-			mColor = LegacyComponentSerializer.legacySection().serialize(LegacyComponentSerializer.legacyAmpersand().deserialize(colorElement.getAsString()));
+			Component colorComp = LegacyComponentSerializer.legacyAmpersand().deserialize(colorElement.getAsString());
+			NamedTextColor found = findColor(colorComp);
+			mTextColor = found != null ? found : NamedTextColor.WHITE;
+			mBold = findBold(colorComp);
 		} else {
 			throw new Exception("times entry missing color value!");
 		}
@@ -71,43 +77,12 @@ public class RaceTime implements Comparable<RaceTime> {
 		return mTime;
 	}
 
-	public String getColor() {
-		return mColor;
-	}
-
-	// TODO: This is an awful translation layer until we update the schema to use NamedTextColor directly
 	public NamedTextColor getTextColor() {
-		for (int i = 0; i < mColor.length() - 1; i++) {
-			if (mColor.charAt(i) == '§') {
-				NamedTextColor color = switch (mColor.charAt(i + 1)) {
-					case '0' -> NamedTextColor.BLACK;
-					case '1' -> NamedTextColor.DARK_BLUE;
-					case '2' -> NamedTextColor.DARK_GREEN;
-					case '3' -> NamedTextColor.DARK_AQUA;
-					case '4' -> NamedTextColor.DARK_RED;
-					case '5' -> NamedTextColor.DARK_PURPLE;
-					case '6' -> NamedTextColor.GOLD;
-					case '7' -> NamedTextColor.GRAY;
-					case '8' -> NamedTextColor.DARK_GRAY;
-					case '9' -> NamedTextColor.BLUE;
-					case 'a', 'A' -> NamedTextColor.GREEN;
-					case 'b', 'B' -> NamedTextColor.AQUA;
-					case 'c', 'C' -> NamedTextColor.RED;
-					case 'd', 'D' -> NamedTextColor.LIGHT_PURPLE;
-					case 'e', 'E' -> NamedTextColor.YELLOW;
-					case 'f', 'F' -> NamedTextColor.WHITE;
-					default -> null; // formatting code (§l, §k, etc.), skip
-				};
-				if (color != null) {
-					return color;
-				}
-			}
-		}
-		return NamedTextColor.WHITE;
+		return mTextColor;
 	}
 
 	public boolean isBold() {
-		return mColor.contains("§l") || mColor.contains("§L");
+		return mBold;
 	}
 
 	@Override
@@ -116,5 +91,31 @@ public class RaceTime implements Comparable<RaceTime> {
 		// less than other, > 0 if this is supposed to be greater than
 		// other and 0 if they are supposed to be equal
 		return mTime < other.mTime ? -1 : mTime == other.mTime ? 0 : 1;
+	}
+
+	private static @Nullable NamedTextColor findColor(Component component) {
+		TextColor color = component.color();
+		if (color instanceof NamedTextColor ntc) {
+			return ntc;
+		}
+		for (Component child : component.children()) {
+			NamedTextColor found = findColor(child);
+			if (found != null) {
+				return found;
+			}
+		}
+		return null;
+	}
+
+	private static boolean findBold(Component component) {
+		if (component.style().decoration(TextDecoration.BOLD) == TextDecoration.State.TRUE) {
+			return true;
+		}
+		for (Component child : component.children()) {
+			if (findBold(child)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
