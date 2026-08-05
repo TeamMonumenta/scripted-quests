e3ca2094acd5fd25e3286b7907a63d122759ae84
fireplace <147332100+Fireplace1544@users.noreply.github.com>
2026-08-05 17:53:09 -0400

    Adds a Race Start Event for usage in main plugin, and calls the event when a race is started

---
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/races/RaceStartEvent.java
@@ -0,0 +1,25 @@
+package com.playmonumenta.scriptedquests.races;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+
+public class RaceStartEvent extends PlayerEvent {
+	private static final HandlerList HANDLERS = new HandlerList();
+
+	private final Race race;
+	private boolean mCancelled = false;
+
+	public RaceStartEvent(Player player, Race race) {
+		super(player);
+		this.race = race;
+	}
+
+	public Race getRace() { return race; }
+
+	@Override
+	public HandlerList getHandlers() { return HANDLERS;	}
+
+	public static HandlerList getHandlerList() { return HANDLERS; }
+}
