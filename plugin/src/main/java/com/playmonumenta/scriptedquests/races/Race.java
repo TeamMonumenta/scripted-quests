f86ff9f6119716639ad83e471491709afae58174
Qwanton <45859836+Qwanton19@users.noreply.github.com>
2026-08-21 14:13:15 -0400

    Update Race.java

--- plugin/src/main/java/com/playmonumenta/scriptedquests/races/Race.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/races/Race.java
@@ -567,8 +567,6 @@ public class Race {
 						RaceUtils.msToTimeString(endTime) +
 						"\",\"color\":\"blue\"}]";
 					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmdStr);
-					String wrStr = "auditlogplayer " + mPlayer.getName() + " \"" + mPlayer.getName() + " has set a new record on " + mName + " with a time of " + RaceUtils.msToTimeString(endTime) + "\"";
-					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), wrStr);
 					Bukkit.getPluginManager().callEvent(new RaceWorldRecordEvent(mPlayer, this, false, endTime));
 				}
 			}
@@ -604,8 +602,6 @@ public class Race {
 					speedScore +
 					"%\",\"color\":\"blue\"}]";
 				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmdStr);
-				String wrStr = "auditlogplayer " + mPlayer.getName() + " \"" + mPlayer.getName() + " has set a new record on " + mName + " with a speed of " + speedScore + "\"";
-				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), wrStr);
 				Bukkit.getPluginManager().callEvent(new RaceWorldRecordEvent(mPlayer, this, true, speedScore));
 			}
 		}
