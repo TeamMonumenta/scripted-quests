642b8dcab89921afaca58c1199b9c1644c54282e
ash <ashenphyxation@gmail.com>
2026-09-07 02:01:49 +0800

    ash/request-28 Timewarp (#361)
    ---------
    
    Co-authored-by: Timothy Southwick <NickNackGus@gmail.com>

--- plugin/src/main/java/com/playmonumenta/scriptedquests/Plugin.java
+++ plugin/src/main/java/com/playmonumenta/scriptedquests/Plugin.java
@@ -110,7 +110,6 @@ public class Plugin extends JavaPlugin {
 		HasPermission.register();
 		TimerDebug.register(this);
 		GenerateCode.register(this);
-		GetDate.register();
 		Code.register(this);
 		SetVelocity.register();
 		Heal.register();
